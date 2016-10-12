4da039f7308cb5309b78fce23099299aa7ae3e06
Malind Nikulski <malind.nikulski@gmail.com>
2016-10-12 22:59:29 +0000

    refactor gem

--- spec/durango_drinks_spec.rb
+++ spec/durango_drinks_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe MicroBreweries do
+describe DurangoDrinks do
   it 'has a version number' do
-    expect(MicroBreweries::VERSION).not_to be nil
+    expect(DurangoDrinks::VERSION).not_to be nil
   end
 
   it 'does something useful' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,2 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'micro_breweries'
+require 'durango_drinks'
