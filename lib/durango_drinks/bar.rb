250f269d5c0d2715d96f582fa8f74af496af377c
Malind Nikulski <malind.nikulski@gmail.com>
2016-10-13 16:44:08 +0000

    build classes

---
+++ lib/durango_drinks/bar.rb
@@ -0,0 +1,29 @@
+class DurangoDrinks::Bar
+  attr_accessor :bar_name, :description, :address, :telephone
+  @@aal = []
+
+  def initialize(attributes = {})
+    @bar_name = attributes[:bar_name]
+    @description = attributes[:description]
+    @address = attributes[:address]
+    @telephone = attributes[:telephone]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.scrape_bars
+    doc = Nokogiri::HTML(open("http://www.durango.org/listings/category/bars-nightlife"))
+    doc.css(".listing_link").each do |listing|
+      bar_name = listing.css("h3").text
+        self.new({#instantiates new Brewery object as iterated through web elements
+        :bar_name => listing.css("h3").text,
+        :description => listing.css(".summary").text,
+        :address => listing.css(".address").text.gsub("\n", " "),
+        :telephone => listing.css(".phone").text
+      })
+    end
+  end
+end
---
+++ lib/durango_drinks/coffee.rb
@@ -0,0 +1,29 @@
+class DurangoDrinks::Coffee
+  attr_accessor :coffee_name, :description, :address, :telephone
+  @@aal = []
+
+  def initialize(attributes = {})
+    @coffee_name = attributes[:coffee_name]
+    @description = attributes[:description]
+    @address = attributes[:address]
+    @telephone = attributes[:telephone]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.scrape_coffeeshops
+    doc = Nokogiri::HTML(open("http://www.durango.org/listings/category/coffee-shops"))
+    doc.css(".listing_link").each do |listing|
+      coffee_name = listing.css("h3").text
+        self.new({#instantiates new Brewery object as iterated through web elements
+        :coffee_name => listing.css("h3").text,
+        :description => listing.css(".summary").text,
+        :address => listing.css(".address").text.gsub("\n", " "),
+        :telephone => listing.css(".phone").text
+      })
+    end
+  end
+end
