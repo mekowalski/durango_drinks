59711952842fe299912ae3b4b4e0db6ccc878eea
Malind Nikulski <malind.nikulski@gmail.com>
2016-10-13 17:23:22 +0000

    refactor gem

--- lib/durango_drinks.rb
+++ lib/durango_drinks.rb
@@ -2,7 +2,9 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require_relative './durango_drinks/version'
+require_relative './durango_drinks/bar'
 require_relative './durango_drinks/brewery'
 require_relative './durango_drinks/cli'
+require_relative './durango_drinks/coffee'
+require_relative './durango_drinks/version'
 #this file acts as environment, requiring all dependencies
